edc0df5120190007c9f591502c6bbeea39247873
francisco <francisco@void-chan.home>
2022-08-23 17:58:33 +0000

    Updated roms, new representations of emulator memory

--- core/src/com/emulator/main/Chip8Emulator.kt
+++ core/src/com/emulator/main/Chip8Emulator.kt
@@ -3,37 +3,27 @@ package com.emulator.main
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.utils.ScreenUtils
+import java.io.File
 import java.util.Timer
 import java.util.Vector
 import kotlin.concurrent.timer
 
 class Chip8Emulator : ApplicationAdapter() {
 
+    private val PC_START = 0x200
+    // TODO: Make choosing a new game interactive
+    private val romFileName = "roms/breakout.ch8"
+
+    private var pc = PC_START
     private var stack = Vector<Int>()
-    private var memory = IntArray(4096, { _ -> 0 })
+    private var memory = ByteArray(4096)
     private var v = IntArray(16, { _ -> 0 })
     private var i = 0
     private var screenPixels = Array(64) { BooleanArray(32) }
 
-    private var oneSecondTime = Timer()
+    private var oneSecondTimer = timerReset()
     private var delay = 60
     private var sound = 60
-
-    private fun timerReset() {
-        this.oneSecondTime =
-                timer(
-                        "OneSecondTimer",
-                        false,
-                        0,
-                        1000,
-                        {
-                            delay--
-                            sound--
-                            if (delay == -1) delay = 60
-                            if (sound == -1) sound = 60
-                        }
-                )
-    }
 
     /*List of things to do:*/
     /*1 - Determine the representation of the emulator parts */
@@ -41,6 +31,11 @@ class Chip8Emulator : ApplicationAdapter() {
     /*3 - Map the codes read on the rom to operations on the data*/
     /* TO FILL */
     override public fun create() {
+        File(romFileName).readBytes().forEachIndexed { index: Int, element: Byte ->
+            memory[index] = element
+        }
+        if (memory == File(romFileName).readBytes()) println("Success") else println("Uh Oh")
+
         timerReset()
     }
 
@@ -48,5 +43,19 @@ class Chip8Emulator : ApplicationAdapter() {
         ScreenUtils.clear(Color.BLACK)
     }
 
+    private fun timerReset(): Timer {
+        return timer(
+                "OneSecondTimer",
+                false,
+                0,
+                1000,
+                {
+                    delay--
+                    sound--
+                    if (delay == -1) delay = 60
+                    if (sound == -1) sound = 60
+                }
+        )
+    }
     override public fun dispose() {}
 }
