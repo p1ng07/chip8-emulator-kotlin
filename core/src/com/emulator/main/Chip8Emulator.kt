0e2b45e0cece47ba3e8e7c6210c8982bf6a1409f
francisco <francisco@void-chan.home>
2022-08-24 16:21:59 +0000

    First 9 opcodes converted

--- core/src/com/emulator/main/Chip8Emulator.kt
+++ core/src/com/emulator/main/Chip8Emulator.kt
@@ -9,6 +9,7 @@ import java.io.File
 import java.util.Timer
 import java.util.Vector
 import kotlin.concurrent.timer
+import kotlin.math.pow
 
 @ExperimentalUnsignedTypes
 class Chip8Emulator : ApplicationAdapter() {
@@ -19,7 +20,9 @@ class Chip8Emulator : ApplicationAdapter() {
         const val SIZE_OF_SQUARE_IN_PIXELS = 15
     }
 
+    // Number of opcodes to be executed per second (HZ)
     val CPU_FREQUENCY = 500
+
     private val PC_START = 0x200
 
     // TODO: Make choosing a new game interactive
@@ -30,7 +33,7 @@ class Chip8Emulator : ApplicationAdapter() {
     private var memory = UByteArray(4096)
     private var v = UByteArray(16)
     private var i = 0
-    private var screenPixels = Array(64) { BooleanArray(32, { _ -> true }) }
+    private var screenPixels = Array(64) { BooleanArray(32, { _ -> false }) }
 
     private var oneSecondTimer = timerReset()
     private var delay = 60
@@ -45,13 +48,14 @@ class Chip8Emulator : ApplicationAdapter() {
         // Implement a full machine restart for dynamic rom selection
         // restartEmulator()
         loadRomToMemory()
+        for (i in fetchCurrentCommand()) println(i)
     }
 
     // We want to execute about 600-1000 opcodes per second, so 15 per timer tick
     override public fun render() {
         ScreenUtils.clear(Color.BLACK)
 
-        for (i in 1..screenData.FPS / CPU_FREQUENCY) executeOpCode(fetchCurrentCommand())
+        // for (i in 1..screenData.FPS / CPU_FREQUENCY) executeOpCode(fetchCurrentCommand())
 
         drawScreen()
     }
@@ -61,16 +65,20 @@ class Chip8Emulator : ApplicationAdapter() {
         val shapeRenderer = ShapeRenderer()
         shapeRenderer.begin(ShapeType.Filled)
         shapeRenderer.setColor(Color.CORAL)
-        for (col in screenPixels.indices) for (row in screenPixels[col].indices) if (screenPixels[
-                        col][row]
-        )
-                shapeRenderer.rect(
-                        (screenData.GAME_X_OFFSET + screenData.SIZE_OF_SQUARE_IN_PIXELS * col)
-                                .toFloat(),
-                        (screenData.SIZE_OF_SQUARE_IN_PIXELS * row).toFloat(),
-                        (screenData.SIZE_OF_SQUARE_IN_PIXELS).toFloat(),
-                        (screenData.SIZE_OF_SQUARE_IN_PIXELS).toFloat()
-                )
+        for (col in screenPixels.indices) {
+            for (row in screenPixels[col].indices) {
+                if (screenPixels[col][row]) {
+
+                    shapeRenderer.rect(
+                            (screenData.GAME_X_OFFSET + screenData.SIZE_OF_SQUARE_IN_PIXELS * col)
+                                    .toFloat(),
+                            (screenData.SIZE_OF_SQUARE_IN_PIXELS * row).toFloat(),
+                            (screenData.SIZE_OF_SQUARE_IN_PIXELS).toFloat(),
+                            (screenData.SIZE_OF_SQUARE_IN_PIXELS).toFloat()
+                    )
+                }
+            }
+        }
         shapeRenderer.end()
     }
 
@@ -81,29 +89,52 @@ class Chip8Emulator : ApplicationAdapter() {
         }
     }
 
-    private fun executeOpCode(array: Array<UByte>) {
-        // https://en.wikipedia.org/wiki/CHIP-8#Opcode_table
-        // when(array[0]){
-        //     1 -> {
-        //         if (array[3] == 0xe) {
+    // Converts n nibbles to Int
+    // start: index of least significance bit where the conversion will start
+    // n: Number of bits to convert from the start
+    private fun nibblesToInt(array: Array<Int>, n: Int, startIndex: Int = 3): UInt {
 
-        //         }
+        if (startIndex > 3 || startIndex < 0) throw Exception("Nibbles to int use was invalid")
+        if (startIndex - n + 1 < 0) throw Exception("Nibbles to int use was invalid")
 
-        //     }
-        //     2 ->
-        //     3 ->
-        //     4 ->
-        //     5 ->
-        //     6 ->
-        // }
+        var res = 0u
+        for (i in startIndex downTo startIndex - n + 1) {
+            res += array[i].toUInt() * 16f.pow(startIndex - i).toUInt()
+        }
+        return res
     }
 
-    private fun fetchCurrentCommand(): Array<UByte> {
-        val array = Array<UByte>(4, { _ -> 0u })
-        array[0] = (memory.get(pc).and(0xF0.toUByte()).toInt() shr 4).toUByte()
-        array[1] = memory.get(pc).and(0x0F.toUByte())
-        array[2] = (memory.get(pc + 1).and(0xF0.toUByte()).toInt() shr 4).toUByte()
-        array[3] = memory.get(pc + 1).and(0x0F.toUByte())
+    private fun executeOpCode(array: Array<Int>) {
+
+        when (array[0]) {
+            0 ->
+                    if (array[3] == 0xE) {
+                        pc = stack.firstElement()
+                        stack.remove(0)
+                    } else screenPixels = Array(64) { BooleanArray(32, { _ -> false }) }
+            1 -> pc = nibblesToInt(array, 3).toInt()
+            2 -> {
+                stack.add(pc)
+                pc = nibblesToInt(array, 3).toInt()
+            }
+            3 -> if (v[array[1]].equals(nibblesToInt(array, 2))) pc += 2
+            4 -> if (!v[array[1]].equals(nibblesToInt(array, 2))) pc += 2
+            5 -> if (v[array[1]].equals(v[array[2]])) pc += 2
+            6 -> v[array[1]] = nibblesToInt(array, 2).toUByte()
+            7 -> v[array[1]].plus(nibblesToInt(array, 2))
+            8 -> v[array[1]] = v[array[2]]
+            9 ->
+        }
+    }
+
+    // Returns the nibbles of the command in memory at index pc
+    // These numbers are also stored Big-Endian (the most significant bit is on the right)
+    private fun fetchCurrentCommand(): Array<Int> {
+        val array = Array<Int>(4, { _ -> 0 })
+        array[0] = memory.get(pc).and(0xF0.toUByte()).toInt() shr 4
+        array[1] = memory.get(pc).and(0x0F.toUByte()).toInt()
+        array[2] = memory.get(pc + 1).and(0xF0.toUByte()).toInt() shr 4
+        array[3] = memory.get(pc + 1).and(0x0F.toUByte()).toInt()
         return array
     }
 
